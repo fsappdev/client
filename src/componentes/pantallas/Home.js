e7044842d14fc451136fded8ff1ae8c8de6adc52
Dario Bnz <developfsa@gmail.com>
2020-12-23 07:36:57 -0300

    falta corregir el isue del borrado del user.

--- src/componentes/pantallas/Home.js
+++ src/componentes/pantallas/Home.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useContext} from "react";
+import {UserContext} from '../../App'
 
 const Home = () => {
   const [data, setData] = useState([])
-  //const jasonToken = localStorage.getItem("jwt")
-  //console.log(jasonToken);
+  const {state, dispatch}= useContext(UserContext)
 
   let myHeaders = new Headers();
     myHeaders.append("Authorization", `Bearer ${localStorage.getItem("jwt")}`)
@@ -21,7 +21,7 @@ const Home = () => {
     fetch("http://localhost:4000/todoslosposts", requestOptions)
       .then(res=>res.json())
       .then(result => {
-        console.log(result)
+        //console.log(result)
         setData(result)
       })
       .catch(error => console.log('error', error));
@@ -38,7 +38,7 @@ const Home = () => {
     fetch("http://localhost:4000/like",requestOptions)
     .then(res=>res.json())
     .then(result => {
-      console.log(result)
+      //console.log(result)
         const newData = data.map(item=>{
           if(item._id==result._id){
               return result
@@ -61,7 +61,7 @@ const Home = () => {
     fetch("http://localhost:4000/dislike",requestOptions)
     .then(res=>res.json())
     .then(result => {
-      console.log(result)
+      //console.log(result)
         const newData = data.map(item=>{
           if(item._id==result._id){
               return result
@@ -74,6 +74,28 @@ const Home = () => {
     .catch(error => console.log('error', error));
   }
 
+  const comentar = (text, postId) => {
+    const requestOptions = {
+      method: 'PUT',
+      body: JSON.stringify({text, postId}),
+      headers: myHeaders,
+      redirect: 'follow' 
+    }
+    fetch("http://localhost:4000/comentar", requestOptions)
+    .then(res=>res.json())
+    .then(result=>{console.log(result)
+      const newData = data.map(item=>{
+        if(item._id==result._id){
+            return result
+        }else{
+            return item
+        }
+      })
+      setData(newData)
+    })
+    .catch(err=>console.log('error', err))
+  }
+
   return (
     <div className="home">
       {
@@ -81,9 +103,9 @@ const Home = () => {
           return(
             
             <div key={item._id} className="card home-card">
-            {console.log(item)}
+            {/* console.log(item) */}
               <div style={{display: "flex", flexwrap: "wrap", justifycontent: "space-around"}} className="row">
-                <h6>Posteado por: </h6><h5> {item.posteadoPor.name}</h5>
+                <small>Posteado por: </small><h6> {item.posteadoPor.name}</h6>
               </div>
                 <div className="card-image">
                   { 
@@ -94,15 +116,40 @@ const Home = () => {
                   /> : <p>El archivo no es una imagen</p>}
                 </div>
               <div className="card-content">
-                <i style={{ color: "red" }} className="material-icons">
+                <i style={{ color: "red" }} className="material-icons pointer">
                   favorite
                 </i>
-                <i onClick={()=>{likePost(item._id)}}className="material-icons">thumb_up</i>
-                <i onClick={()=>{disLikePost(item._id)}}className="material-icons">thumb_down</i>
+                <i style={{ color: "green" }} className="material-icons pointer">
+                  save
+                </i>     
+                {item.likes.includes(state._id) ? <i onClick={()=>{disLikePost(item._id)}}className="material-icons pointer">thumb_down</i> : <i onClick={()=>{likePost(item._id)}}className="material-icons pointer">thumb_up</i> }
+                
                 <h6>{item.likes ? item.likes.length : 0} Likes </h6>
                 <h5>{item.titulo}</h5>
-                <p>{item._id}</p>
-                <input type="text" placeholder="agrega un comentario" />
+                <h6>{item.cuerpo}</h6>
+
+                {
+                  item.comentarios ? item.comentarios.map(comentario => {
+                    return (
+                      <h6 key={comentario._id} style={{fontWeight:"500"}}>
+                      {comentario.posteadoPor.name} = 
+                        <span >
+                          {comentario.text}
+                        </span>
+                        
+                      </h6>
+                    )
+                  }) : null
+                }
+
+                <form onSubmit={(e) => {
+                  e.preventDefault()
+                  //console.log(e.target[0].value)
+                  comentar(e.target[0].value,item._id)
+                }}>
+                  <input type="text" placeholder="agrega un comentario" />
+                </form>
+                
               </div>
             </div>
           )
